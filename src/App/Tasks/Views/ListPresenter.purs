3050a0cfd153673cb82fb3bd92635a4b144d822b
Rouan van Dalen <rouan.van.dalen@gmail.com>
2018-07-30 11:19:33 +0200

    - added task list view.

--- src/App/Tasks/Views/ListPresenter.purs
+++ src/App/Tasks/Views/ListPresenter.purs
@@ -1,19 +1,32 @@
 module App.Tasks.Views.ListPresenter
 ( Presenter
 , mkPresenter
+, addTask
 ) where
 
 import Prelude
+import Data.Array (snoc)
+import Effect (Effect)
 import Effect.Ref (Ref)
 import Effect.Ref as Ref
 import Webfu.Data.Err (Err(..))
 import App.Tasks.Models (Task)
+import App.GlobalState as GlobalState
 
 
 type Presenter =
-  {}
+  { newTaskText :: String
+  }
 
 
 mkPresenter :: Unit -> Presenter
 mkPresenter initState =
-  {}
+  { newTaskText: ""
+  }
+
+
+addTask :: Ref Presenter -> Effect Unit
+addTask refPr = do
+  pr <- Ref.read refPr
+  let task = { title: pr.newTaskText, description: "" }
+  Ref.modify_ (\tasks -> snoc tasks task ) GlobalState.tasks
--- src/App/Tasks/Views/ListView.purs
+++ src/App/Tasks/Views/ListView.purs
@@ -3,17 +3,23 @@ module App.Tasks.Views.ListView
 ) where
 
 import Prelude
+import Data.Maybe (Maybe(..), maybe)
+import Data.Array as Array
 import Effect (Effect)
 import Effect.Console (log)
 import Effect.Ref (Ref)
 import Effect.Ref (read, write) as Ref
-import Data.Maybe (Maybe(..), maybe)
 import Webfu.Data.Err (Err(..))
 import Webfu.DOM
 import Webfu.DOM.Promise
-import Webfu.Mithril (Component, mkComponent, raise, redraw)
+import Webfu.DOM.Events (KeyboardEvent)
+import Webfu.DOM.Events.Keyboard (key)
+import Webfu.Mithril (Component, VNode, mkComponent, raise, redraw)
 import Webfu.Mithril.HTML
-import App.Tasks.Views.ListPresenter (Presenter)
+import App.GlobalState as GlobalState
+import App.Tasks.Models (Task)
+import App.Tasks.Views.ListPresenter (Presenter, addTask)
+
 
 -----------------------------------------------------------
 -- STATE
@@ -25,9 +31,26 @@ import App.Tasks.Views.ListPresenter (Presenter)
 -----------------------------------------------------------
 data Msg
   = AddTask
+  | OnEnterKey KeyboardEvent
 
 update :: Ref Presenter -> Msg -> Effect Unit
 update refPr AddTask = pure unit
+  -- maybeTaskTitle <- lookupValue "taskInput"
+  -- case maybeTaskTitle of
+  --   Nothing -> pure unit
+  --   Just t  -> addTask t
+
+update refPr (OnEnterKey e) =
+  if (key e) == "Enter"
+    then log "Enter baby"
+    else pure unit
+
+
+lookupValue :: String -> Effect (Maybe String)
+lookupValue elemId = do
+  elem <- doc_getElementById elemId
+  pure $ el_prop_value =<< elem
+
 
 
 -----------------------------------------------------------
@@ -40,10 +63,20 @@ raiseEvent refSt = raise update refSt
 
 mkView :: Presenter -> Component
 mkView presenter =
-  mkComponent presenter (\refPr _ -> do
-    p <- Ref.read refPr
+  mkComponent presenter (\refSt _ -> do
+    p <- Ref.read refSt
+    tasks <- Ref.read GlobalState.tasks
     pure $ main [] [
       h1' ["style":="color:red;"] "TODOs",
-      input [],
-      button' ["onclick":= \_ -> raiseEvent refPr AddTask] "++"
+      input ["type":="text", "id":="taskInput", onKeyPress \e -> pure $ raiseEvent refSt (OnEnterKey e)],
+      button' ["onclick":= \_ -> raiseEvent refSt AddTask] "Add",
+      table [] [
+        tbody [] (map taskToRow tasks)
+        ]
       ])
+
+taskToRow :: Task -> VNode
+taskToRow t =
+  tr [] [
+    td' [] t.title
+    ]
